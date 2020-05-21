b1d370ed38748c4313171854179f3795d9a3e860
GeeTransit <geetransit@gmail.com>
2020-05-21 14:53:20 -0400

    Add logging
    - console: morgan, debug
    - file: morgan, rotating-file-stream

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 // from: https://expressjs.com/en/starter/hello-world.html
 
 const express = require("express")
+const debug = require("debug")("chatapp")
+const morgan = require("morgan")
+const path = require("path")
+const rfs = require("rotating-file-stream")
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+const accessLogStream = rfs.createStream("access.log", {
+	size: "1M",
+	interval: "1d",
+	path: path.join(__dirname, "log")
+})
+app.use(morgan("combined", {stream: accessLogStream}))
+app.use(morgan("short", {stream: process.stdout}))
 
 app.get("/", (req, res) => res.send("Hello, world!"))
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+app.listen(port, _ => debug(`App listening at http://localhost:${port}`))
